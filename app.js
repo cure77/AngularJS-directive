598c85c0daf9402c5476463506cb1b5183d709d0
Ljubisav Curcic <ljubisav.curcic@gmail.com>
2018-11-05 00:41:45 +0100

    - added date picker range

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ angular.module('app', ['ui.bootstrap'])
 
 angular.module('app').controller('directivesCtrl', function ($scope) {
 
+	$scope.viewEdit = 'edit';
+
 	$scope.formData = {
 		dt: {
 			begin: new Date(),
@@ -16,7 +18,9 @@ angular.module('app').directive('fsmDatePicker', function(){
 	return {
 		templateUrl: 'datePicker.html',
 		scope: {
-			namedp: '='
+			namedp: '=',
+			requiredp: '=',
+			modedp: '='
 		},
 		controller: function($scope, $element, $attrs) {
 		
@@ -36,5 +40,46 @@ angular.module('app').directive('fsmDatePicker', function(){
 			};
 		}
 	}
+});
 
+angular.module('app').directive('fsmDatePickerRange', function(){
+	return {
+		templateUrl: 'datePickerRange.html',
+		scope: {
+			namedpfrom: '=',
+			namedpto: '=',
+			requiredp: '=',
+			modedp: '='
+		},
+		controller: function($scope, $element, $attrs) {
+		
+			$scope.fromName = $attrs['namedpfrom'];
+			$scope.toName = $attrs['namedpto'];
+
+			$scope.dateOptions = {
+				format: 'dd/MM/yyyy',
+				//ngModelOptions : {timezone: 'utc'},
+				formatYear: 'yyyy',
+				startingDay: 1,
+				openCloseStatus: {},
+				showWeeks: false
+			};
+		
+			$scope.dateToOptions = $scope.dateOptions; // because we will add different min date dynamically after picking date from
+		
+			$scope.$watch('namedpfrom', function(newValue, oldValue) {
+			  if (newValue !== oldValue) {
+				if ($scope.namedpto !== undefined && newValue > $scope.namedpto){
+				  delete $scope.namedpto;
+				}
+				$scope.dateToOptions.minDate = $scope.namedpfrom;
+			  }
+			});
+		
+			$scope.openClose = function(fieldName) {
+				$scope.dateOptions.openCloseStatus[fieldName] = !$scope.dateOptions.openCloseStatus[fieldName];
+			};
+			
+		}
+	}
 });
